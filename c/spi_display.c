fbe8e6eab5f7a33285e2ad6ba201cd680d1f59eb
Richard Ribier <ribier@gmail.com>
2015-12-13 08:50:18 +0000

    c driver pour spi_lcd

--- c/spi_display.c
+++ c/spi_display.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 #include <sys/mman.h>
@@ -73,16 +74,95 @@ void unmap_peripheral(struct bcm2835_peripheral *p) {
 }
 
 void send_command(unsigned char cmd){
+    GPIO_CLR = 1 << SPIDIO;
+    GPIO_SET = 1 << SPICLK;
+    usleep(150);
+    GPIO_CLR = 1 << SPICLK;
+    for(int i=0;i<8;i++){
+        if((cmd>>(7-i))&0x1){
+            GPIO_SET = 1 << SPIDIO;
+        }
+        else{
+            GPIO_CLR = 1 << SPIDIO;
+        }
+        GPIO_SET = 1 << SPICLK;
+        usleep(150);
+        GPIO_CLR = 1 << SPICLK;
+    }
+}
 
+void send_data(unsigned char data){
+    GPIO_SET = 1 << SPIDIO;
+    GPIO_SET = 1 << SPICLK;
+    usleep(150);
+    GPIO_CLR = 1 << SPICLK;
+    for(int i=0;i<8;i++){
+        if((data>>(7-i))&0x1){
+            GPIO_SET = 1 << SPIDIO;
+        }
+        else{
+            GPIO_CLR = 1 << SPIDIO;
+        }
+        GPIO_SET = 1 << SPICLK;
+        usleep(150);
+        GPIO_CLR = 1 << SPICLK;
+    }
+}
+
+void reset_lcd(){
+    GPIO_CLR = 1 << SPIRES;
+    usleep(2000);
+    GPIO_SET = 1 << SPIRES;
+    usleep(2000);
+    send_command(0xCA);
+    send_data(0x00);
+    send_data(0x20);
+    send_data(0x00);
+    send_command(0xBB);
+    send_data(0x01);
+    send_command(0xD1);
+    send_command(0x94);
+    send_command(0x20);
+    send_data(0x0f);
+    send_command(0xA7);
+    send_command(0xBC);
+    send_data(0x01);
+    send_data(0x00);
+    send_data(0x02);
+    send_command(0x81);
+    send_data(32);
+    send_data(3);
+    usleep(2000);
+    send_command(0xAF);
+}
+
+void fill_rect(unsigned char x1,unsigned char x2,unsigned char y1,unsigned char y2,char r,char g, char b){
+    send_command(0x75);
+    send_data(x1);
+    send_data(x2);
+    send_command(0x15);
+    send_data(y1);
+    send_data(y2);
+    int l = (x2-x1)*(y2-y1);
+    send_command(0x5C);
+    unsigned char byte1 = r<<4|g;
+    unsigned char byte2 = b<<4|r;
+    unsigned char byte3 = g<<4|b;
+    for (int i=0;i<(l/2);i++){ 
+        send_data(byte1);
+        send_data(byte2);
+        send_data(byte3);
+    }
 }
 
 int main(){
+    
     if(map_peripheral(&gpio) == -1){
         printf("INIT FAILED !!\n");
         return -1;
     }
 
-    //setup out outputs
+    //setup outputs
     INP_GPIO(21); //SPICLK
     OUT_GPIO(21); //SPICLK
     INP_GPIO(24); //SPIDIO
@@ -94,6 +174,10 @@ int main(){
 
     INP_GPIO(25); //SPICLK
     OUT_GPIO(25); //SPICLK
+    
+    reset_lcd();
+    fill_rect(0,80,0,80,0xFF,0x00,0x00);
+
 
     while(1) {
         GPIO_SET = 1<<25;
